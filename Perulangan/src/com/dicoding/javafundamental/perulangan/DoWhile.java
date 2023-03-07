efa4ad984869ebfaed976275a45a38459c76e405
Artiko F Samodra <48190353+artikosamodra@users.noreply.github.com>
2023-03-07 09:02:04 +0700

    Update Java Class - Percabangan

---
+++ Perulangan/src/com/dicoding/javafundamental/perulangan/DoWhile.java
@@ -0,0 +1,14 @@
+package com.dicoding.javafundamental.perulangan;
+
+public class DoWhile {
+    public static void main(String[]args){
+        int value = 1;
+
+        do{
+            System.out.println("Angka : "+value);
+            value++;
+        } while (value <=10);
+
+        //do = melakukan perintah perulangan hingga kondisi memenuhi while, perulangan berhenti
+    }
+}
