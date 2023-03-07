df6cf36dbd061976e807d5944506ed895cd5a946
Artiko F Samodra <48190353+artikosamodra@users.noreply.github.com>
2023-03-07 20:20:54 +0700

    Update Java - Interface

---
+++ Interface/src/com/dicoding/javafundamental/oop/Hewan.java
@@ -0,0 +1,9 @@
+package com.dicoding.javafundamental.oop;
+
+public interface Hewan {
+    String RESPIRASI = "oksigen";
+    void makan();
+    //{
+    //    System.out.println("Makan daging, tumbuhan, atau segalanya");
+    //}
+}
---
+++ Interface/src/com/dicoding/javafundamental/oop/Main.java
@@ -0,0 +1,8 @@
+package com.dicoding.javafundamental.oop;
+
+public class Main {
+    public static void main (String []args){
+        System.out.println("Bernafas Menggunakan "+ Hewan.RESPIRASI);
+
+    }
+}
